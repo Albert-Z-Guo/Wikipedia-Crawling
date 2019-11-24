6214e2cae5ca51ca26cac0c06fe57dedd72e856d
Albert Guo <albertguotrs@hotmail.com>
2019-11-23 21:49:34 -0600

    clean and optimize code

--- crawl_wikipedia_biographies.py
+++ crawl_wikipedia_biographies.py
@@ -4,59 +4,83 @@ import time
 
 import requests
 import wikipedia
+import numpy as np
 
 
 '''
-# retrieve people's names
-# reference: https://query.wikidata.org
+# reference: 
+# https://query.wikidata.org
+
+# retrieve articles of people (born after 1500 and die before 2010) who received an award
 query = """
-SELECT DISTINCT ?article ?award ?person ?birth ?death WHERE {  
+SELECT DISTINCT ?article WHERE {  
   ?award (wdt:P31/(wdt:P279*)) wd:Q618779. # instance or subclass of an award
-  ?person wdt:P166 ?award ; # person received an award
-          wdt:P31 wd:Q5. # person is instance of human
-  
-  ?article schema:about ?person.
-  ?article schema:isPartOf <https://en.wikipedia.org/>. # article is in English
+  ?person wdt:P31 wd:Q5; # person is instance of human
+          wdt:P166 ?award. # person received an award
   
   ?person wdt:P569 ?birth;
           wdt:P570 ?death.
   
-  FILTER((?birth > "1910-01-01"^^xsd:dateTime) && (?death < "2010-01-01"^^xsd:dateTime))
+  ?article schema:about ?person.
+  ?article schema:isPartOf <https://en.wikipedia.org/>. # article is in English
+  
+  FILTER((?birth > "1900-01-01"^^xsd:dateTime) && (?death < "2010-01-01"^^xsd:dateTime))
 }
-LIMIT 400000
 """
+
+# retrieve articles of people (born after 1500 and die before 2010) who are scientists
+query = """
+SELECT DISTINCT ?article WHERE {
+	?person wdt:P31 wd:Q5; # person is instance of human
+            wdt:P106/wdt:P279* wd:Q901. # person is a scientist
+
+    ?article schema:about ?person.
+    ?article schema:isPartOf <https://en.wikipedia.org/>. # article is in English
+  
+    ?person wdt:P569 ?birth;
+            wdt:P570 ?death.
+    FILTER((?birth > "1500-01-01"^^xsd:dateTime) && (?death < "2010-01-01"^^xsd:dateTime))
+}
+"""
+
 url = 'https://query.wikidata.org/bigdata/namespace/wdq/sparql'
 json_data = requests.get(url, params={'query':query, 'format':'json'}).json()
 with open('people_test.json', 'w') as outfile:
     json.dump(json_data['results']['bindings'], outfile)
 '''
 
-with open('people_test.json') as json_file:
+# filename = 'people_won_awards_1910_2010_en'
+filename = 'scientists_1500_2010_en'
+with open('{}.json'.format(filename)) as json_file:
     data = json.load(json_file)
-print('raw data length:', len(data))
+print('data length:', len(data))
 
 
-# count number of unique figure
-people = set()
+# count number of unique articles
+articles = set()
 data_deduplicated = []
 for item in data:
-    if item['person'] not in people:
-        people |= {item['person']}
+    if item['article'] not in articles:
+        articles |= {item['article']}
         data_deduplicated.append(item)
-print('number of unique figure:', len(people))
+print('number of unique articles:', len(articles))
 
 
-def retrieve_content(data):
+# select 20000 random articles
+data_selected = [data_deduplicated[i] for i in np.random.choice(20000, len(articles))]
+
+
+def retrieve_content(data, directory_name):
     article_url = data['article']
     article_name = article_url.split('/')[-1]
     print(article_name, '|', article_url)
     
     # check if file exists
-    if os.path.isfile('crawled_biographies/{}.txt'.format(article_name)):
+    file_path = '{}/{}.txt'.format(directory_name, article_name)
+    if os.path.isfile(file_path):
         print('{}.txt exists'.format(article_name))
     else:
-        api_response_json = requests.get('https://en.wikipedia.org/w/api.php?action=query&format=json&titles={}'
-                               .format(article_name)).json()
+        api_response_json = requests.get('https://en.wikipedia.org/w/api.php?action=query&format=json&titles={}'.format(article_name)).json()
         try:
             content = wikipedia.page(title=list(api_response_json['query']['pages'].values())[0]['title']).content
         except Exception as e:
@@ -67,18 +91,17 @@ def retrieve_content(data):
             except Exception as e:
                 content = ''
                 print(e, 'alternative method failed!')
-        
         # save file
-        filename = 'crawled_biographies/{}.txt'.format(article_name)
-        os.makedirs(os.path.dirname(filename), exist_ok=True)
-        with open(filename, 'w') as file:
+        os.makedirs(os.path.dirname(file_path), exist_ok=True)
+        with open(file_path, 'w') as file:
             file.write(content)
 
 
-start = 0
-for i in range(start, len(data_deduplicated)):
+start = 20
+end = len(data_selected)
+for i in range(start, end):
     print(i, end=' ')
-    retrieve_content(data_deduplicated[i])
+    retrieve_content(data_selected[i], filename)
     i += 1
     if i % 5 == 0:
         time.sleep(1)
